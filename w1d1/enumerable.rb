5756079a511fd526915bf4f04f529e1fb07bb350
Krista Jackson <TheAxnJaxn@users.noreply.github.com>
2015-05-28 15:28:24 -0700

    Create enumerable.rb

---
+++ w1d1/enumerable.rb
@@ -0,0 +1,38 @@
+def double(array)
+  array.map { |num| num * 2 }
+end
+
+class Array
+  
+  def my_each
+    
+    idx = 0
+    while idx < self.length
+      yield self[idx]
+      idx += 1
+    end
+    
+    return self
+    
+  end
+
+end
+
+def median(array)
+
+  array = array.sort
+
+  if array.count.even?
+    idx1 = (array.count/2 - 1)
+    idx2 = (array.count/2)
+    return ((array[idx1].to_f + array[idx2].to_f)/2)
+  else
+    idx = (array.count/2)
+    return array[idx]
+  end
+
+end
+
+def all_together(array)
+  array.inject(:+)
+end
