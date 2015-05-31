0f7f6a29ff6eecc85d11b9723e510ee68c3a9359
Krista Jackson <TheAxnJaxn@users.noreply.github.com>
2015-05-31 00:11:26 -0700

    Update 02_calculator.rb
    Update with responses to bonus sections

--- w1d5/lib/02_calculator.rb
+++ w1d5/lib/02_calculator.rb
@@ -10,3 +10,19 @@ def sum(array)
   if array.empty? then return 0 end
   return array.inject(:+)
 end
+
+def multiply(*nums)
+  return nums.inject(1) {|product, n| product * n}
+end
+
+def power(num1, num2)
+  return num1 ** num2
+end
+
+def factorial(num)
+  if num == 1 || num == 0
+    return 1
+  else
+    return num * factorial(num - 1)
+  end
+end
