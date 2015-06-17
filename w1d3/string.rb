ea098f74ecd1844d82c23408655a5f4257f08779
Krista Jackson <TheAxnJaxn@users.noreply.github.com>
2015-06-16 20:36:38 -0700

    Update string.rb
    Added num_to_s method

--- w1d3/string.rb
+++ w1d3/string.rb
@@ -1,3 +1,17 @@
+def num_to_s(num, base)
+  string_version = ""
+  divide_by = 1
+  string_digits = { 0 => "0", 1 => "1", 2 => "2", 3 => "3", 4 => "4", 5 => "5", 6 => "6", 7 => "7", 8 => "8", 9 => "9", 10 => "A", 11 => "B", 12 => "C", 13 => "D", 14 => "E", 15 => "F" }
+  
+  until num / divide_by == 0
+    string_version << string_digits[(num / divide_by) % base]
+    divide_by *= base
+  end
+  
+  return string_version.reverse
+  
+end
+
 def caesar(string, num)
   
   idx = 0
